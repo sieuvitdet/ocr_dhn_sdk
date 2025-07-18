24350798ff11d28e06462c093b2e3bed345273ea
Tran Tan Duc <trantanduc93@gmail.com>
2025-07-18 09:12:38 +0700

    push

--- lib/water_meter_sdk.dart
+++ lib/water_meter_sdk.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'water_meter_sdk_platform_interface.dart';
 import 'services/water_meter_ocr_service.dart';
 import 'models/water_meter_result.dart';
@@ -12,8 +14,8 @@ class WaterMeterSdk {
 
   /// Process an image file to extract water meter reading
   /// [imagePath] should be a valid path to an image file
-  Future<WaterMeterResult> processWaterMeterImage(String imagePath) async {
-    return await _ocrService.processImage(imagePath);
+  Future<WaterMeterResult> processWaterMeterImage(Uint8List imageBytes, {String? imageFull}) async {
+    return await _ocrService.processImage(imageBytes, imageFull: imageFull);
   }
 
   /// Dispose of resources
